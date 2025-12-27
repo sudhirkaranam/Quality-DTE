19994e2ed2023f093be29459828275896fffe92a
sudhirkaranam <sudhir.karanam@gmail.com>
2025-12-27 11:03:53 +0530

    Updated app with timeout handling and sector-specific filters

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import requests
 # Setup non-interactive plotting
 matplotlib.use('Agg')
 
-st.set_page_config(page_title="Stock DTE Meter qaaa", layout="wide")
+st.set_page_config(page_title="Stock DTE Meter 11", layout="wide")
 
 # --- Initialize Session State ---
 if 'processed_results' not in st.session_state:
